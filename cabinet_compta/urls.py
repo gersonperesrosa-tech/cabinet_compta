26656ef585919547dc0e8b2202b262e5c06bc642
gersonperesrosa-tech <gerson.peres.rosa@gmail.com>
2026-02-27 12:37:53 +0100

    Fix static files config for Render deployment

--- cabinet_compta/urls.py
+++ cabinet_compta/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from dossiers.views import CustomLoginView  # <-- importe ta vue
+from dossiers.views import CustomLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Page d'accueil = page de login
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('', CustomLoginView.as_view(), name='login'),
-
-    # Routes existantes
     path('dossiers/', include('dossiers.urls')),
     path('paie/', include('paie.urls')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
